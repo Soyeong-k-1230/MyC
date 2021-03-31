1be688c6a94ab042756f6d2a0b1045c3dc9f6e57
soyeong kim <soyeong.kim.95@gmail.com>
2021-04-01 02:22:14 +0900

    heroku deploy
    Signed-off-by: soyeong kim <soyeong.kim.95@gmail.com>

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MyC.wsgi --log-file -
--- final_pjt/settings.py
+++ final_pjt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n0_ob+9v8h#uw(39lhzgrzqn$z*qy!jop5uwbnzhzg58y0(3zd'
+# SECRET_KEY = 'n0_ob+9v8h#uw(39lhzgrzqn$z*qy!jop5uwbnzhzg58y0(3zd'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n0_ob+9v8h#uw(39lhzgrzqn$z*qy!jop5uwbnzhzg58y0(3zd')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', False))
 
 ALLOWED_HOSTS = [
 
@@ -69,6 +72,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'final_pjt.urls'
@@ -95,12 +99,15 @@ WSGI_APPLICATION = 'final_pjt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -138,6 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
