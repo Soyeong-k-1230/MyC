b10a669f49a7b236173c5b62f4d3d91a5b84a429
soyeong kim <soyeong.kim.95@gmail.com>
2021-04-01 03:31:02 +0900

    edit setting.py
    Signed-off-by: soyeong kim <soyeong.kim.95@gmail.com>

--- final_pjt/settings.py
+++ final_pjt/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,14 +158,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # ROOT_DIR = os.path.dirname(BASE_DIR)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
 # STATIC_ROOT = os.path.join(ROOT_DIR, 'static_root')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 
 AUTH_USER_MODEL = 'accounts.User'
@@ -206,6 +206,6 @@ SOCIALACCOUNT_PROVIDERS = {
 LOGIN_REDIRECT_URL = '/movies/'
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
 
 
