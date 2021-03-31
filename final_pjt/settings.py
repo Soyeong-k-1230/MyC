03178d77e31344f258e9b3bb174d37f14b778b19
soyeong kim <soyeong.kim.95@gmail.com>
2021-04-01 03:35:08 +0900

    collectstatic
    Signed-off-by: soyeong kim <soyeong.kim.95@gmail.com>

--- final_pjt/settings.py
+++ final_pjt/settings.py
@@ -158,8 +158,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # ROOT_DIR = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
